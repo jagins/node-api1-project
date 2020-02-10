ac0a511d1b60a16342857f904e7cddf421edbf13
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-10 13:33:05 -0600

    server is up and running

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) =>
+{
+    res.json({message: 'Its working!!'});
+})
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server is working on localhost:${PORT}`));
