6c15fe53a29a0da20c5044d96f0e5ca56b5a469c
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-10 14:27:05 -0600

    get users by id is done and works. returns error message if user is not found

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const database = require('./data/db');
 
 const server = express();
 
@@ -7,6 +8,39 @@ server.use(express.json());
 server.get('/', (req, res) =>
 {
     res.json({message: 'Its working!!'});
+})
+
+server.get('/api/users', (req, res) =>
+{
+    database.find()
+    .then(users =>
+    {
+        res.status(200).json(users);
+    })
+    .catch(error =>
+    {
+        res.status(500).json({errorMessage: 'The users information could not be retrieved.'})
+    })
+})
+
+server.get('/api/users/:id', (req, res) =>
+{
+    database.findById(req.params.id)
+    .then(user =>
+    {
+        if(!user)
+        {
+            res.status(404).json({message: 'The user with that specified ID does not exist'});
+        }
+        else
+        {
+            res.status(200).json(user);
+        }
+    })
+    .catch(error =>
+    {
+        res.status(500).json({errorMessage: 'The users information could not be retrieved.'})
+    })
 })
 
 const PORT = 5000;
