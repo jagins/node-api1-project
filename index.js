1d20931c7bb405eccce24f1f341d8909c46537d5
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-10 15:35:23 -0600

    update works and returns the updated user

--- index.js
+++ index.js
@@ -93,6 +93,42 @@ server.delete('/api/users/:id', (req, res) =>
     })
 })
 
+server.put('/api/users/:id', (req, res) =>
+{
+    database.findById(req.params.id)
+    .then(user =>
+    {
+        if(!user)
+        {
+            res.status(404).json({message: 'The user with the specified ID does not exist'});
+        }
+        else
+        {
+            const updateUser = req.body;
+            if(!updateUser.name || !updateUser.bio)
+            {
+                res.status(400).json({errorMessage: 'Please provide a name and bio for the user'});
+            }
+            else
+            {
+                database.update(user.id, updateUser)
+                .then(updatedUser =>
+                {
+                    res.status(200).json(updateUser);
+                })
+                .catch(error =>
+                {
+                    res.status(500).json({errorMessage: 'the user information could not be modified'});
+                })
+            }
+        }
+    })
+    .catch(error =>
+    {
+        res.status(500).json({errorMessage: 'The users information could not be retrieved'});
+    })
+})
+
 const PORT = 5000;
 
 server.listen(PORT, () => console.log(`Server is working on localhost:${PORT}`));
